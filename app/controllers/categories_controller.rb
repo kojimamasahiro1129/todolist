57addd6970350668523b6361881e81b4f9c9c834
EC2 Default User <ec2-user@ip-172-31-73-13.ec2.internal>
2020-12-31 03:46:54 +0000

    カテゴリー幅

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,22 @@
 class CategoriesController < ApplicationController
     def index
         @category = Category.new
+        @categories = Category.where.not(name: nil)
     end
+    
+    def create
+        category = Category.new(category_paramater)
+       if category.save
+           redirect_to root_path
+       else
+           p "jasdkfjadf"
+       end
+    end
+end
+
+
+private
+
+def category_paramater
+    params.require(:category).permit(:name)
 end

5bf05bc0c52a5d877bfe25b5626da4538480b280
EC2 Default User <ec2-user@ip-172-31-73-13.ec2.internal>
2021-01-04 09:42:01 +0000

    logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+       log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+
       # 保存の成功をここで扱う。
       redirect_to @user
     else
